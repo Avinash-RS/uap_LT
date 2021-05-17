46adc2bba641492dfec7f159190fb5a891c7f08d
Afser Ali <AFSER-ALI@lntecc.com>
2021-05-17 07:20:35 +0000

    Updated environment.staging.ts

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   API_URL: '<specify-end-point>',
   OIDC_CONFIG: {
-    issuer: 'https://iam-staging.skiquo.io/auth/realms/kodepro',
+    issuer: 'https://uap-iam.lntiggnite.com/auth/realms/kodepro',
     clientId: 'uap-ui',
     logging : LogLevel.Error
   }
