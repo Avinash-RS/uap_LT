360ea340e54bfbd518518bc148c63d94ae41d591
vidwath <vidwath_pampapathi@intuit.com>
2021-05-29 19:16:30 +0530

    UI task design changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,11 @@ export const environment = {
   production: false,
   API_URL: 'https://uap-api.lntiggnite.com',
   OIDC_CONFIG: {
-    issuer: 'https://uap-iam.lntiggnite.com/auth/realms/uap',
+    issuer: 'https://uap-iam.lntiggnite.com/auth/realms/kodepro',
     clientId: 'uap-ui',
     logging: LogLevel.Error
   },
-  TOKEN_URL: 'https://uap-iam.lntiggnite.com/auth/realms/uap/protocol/openid-connect/token'
+  TOKEN_URL: 'https://uap-iam.lntiggnite.com/auth/realms/kodepro/protocol/openid-connect/token'
 };
 
 /*
