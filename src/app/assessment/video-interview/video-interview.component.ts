9cc4908313b4d229bf712524149d34aaf2280b8f
B-ANKIT <b-ankit@lntecc.com>
2021-11-17 19:22:42 +0530

    Commit with timer issue fixed and excel upload field validation

--- src/app/assessment/video-interview/video-interview.component.ts
+++ src/app/assessment/video-interview/video-interview.component.ts
@@ -7,6 +7,7 @@ import { AssessmentAPIService } from 'src/app/rest-api/assessments-api/assessmen
 import _ from 'lodash';
 import { AssessmentTaskUrlModel } from 'src/app/rest-api/assessments-api/models/assessment-task-url-response-model';
 import { LoadingService } from 'src/app/rest-api/loading.service';
+import * as moment from 'moment';
 @Component({
   selector: 'uap-video-interview',
   templateUrl: './video-interview.component.html',
@@ -37,8 +38,8 @@ export class VideoInterviewComponent implements OnInit {
   lastQusDetails: any;
   isStartEnable = false;
   isNextBtn = true;
-  taskUrlData: AssessmentTaskUrlModel;
   displayQus: any;
+  qusTime: string;
   constructor(private _loading: LoadingService,private route: Router,private dialog: MatDialog, private toast: ToastrService,private http : AssessmentAPIService) {
     this.userProfile = JSON.parse(sessionStorage.getItem('user'));
     sessionStorage.setItem('activequs',this.activequs)
@@ -65,6 +66,7 @@ export class VideoInterviewComponent implements OnInit {
         } else {
           this.firstQusTime =  this.qusInfo[0].questionDetails.duration;
           this.timeLeft = this.firstQusTime * 60;
+          this.getQusDuration(this.timeLeft)
         }
       }else {
         this.toast.warning('Please try after sometime...')
@@ -82,28 +84,11 @@ export class VideoInterviewComponent implements OnInit {
    let val =  _.filter(matchLastQus, {_id: this.lastQusDetails.id}); // get last qus 
    this.countdownStart = parseInt(this.lastQusDetails.duration);
    this.timeLeft = parseInt(val[0].duration) * 60 - this.countdownStart;
-  //  console.log(val[0])
    this.displayQus =  val[0].question;
    let index = matchLastQus.findIndex(item => item._id === this.lastQusDetails.id); // find qus index
-  //  console.log(index,'index')
    this.activequs = index;
    sessionStorage.setItem('activequs',this.activequs)
   }
-
-  // getTime(duration,index,qusLength,activequs){
-  //   this.activequs = index; // display active qus button
-  //   this.isStartbtn = true; // start button enable disable
-  //   this.timeLeft = parseInt(duration) * 60;  // convert mins into secx
-  //   this.countdownStart = 1;
-  //   this.qusDetails = this.qusInfo[activequs].questionDetails._id;
-  //   this.nextQusId = this.qusInfo[activequs+ 1].questionDetails._id;
-  //   this.qusDuration = duration * 60;
-  //   if(qusLength -1 == index){
-  //     // this.actions('submit',false,activequs);
-  //   }else{
-  //     this.actions('next', false,activequs);
-  //   }
-  // }
 
 
   startRecord(activequs,stauts,isStart){
@@ -116,24 +101,21 @@ export class VideoInterviewComponent implements OnInit {
     }
     this.qusDetails = this.qusInfo[activequs].questionDetails._id;
     this.isStartbtn = false;  // disable start button
+    sessionStorage.setItem('activequs',this.activequs)
     this.actions(stauts,true,activequs);
 }
 
  
   nextQus(nextqus,status){
-    console.log(this.activequs,'this.activequs')
     // this.isNextBtn = true;
       if(this.isRecordStarted == true){
             this.isRecordStarted = false;
             this.qusEndDate = new Date();
-
       }else{
         this.qusStartDate = '';
       }
-
-    this.isStartbtn = true; 
+    // this.isStartbtn = true; 
     if(this.qusInfo.length > this.activequs){
-
       this.activequs = parseInt(this.activequs + 1) ;
       this.qusDetails = this.qusInfo[this.activequs].questionDetails._id;
       this.nextQusId = this.qusInfo[this.activequs].questionDetails._id;
@@ -141,75 +123,39 @@ export class VideoInterviewComponent implements OnInit {
       sessionStorage.setItem('activequs',this.activequs)
       this.displayQus = this.qusInfo[this.activequs].questionDetails.question
       this.timeLeft =  this.qusInfo[this.activequs].questionDetails.duration * 60;
-      
+      this.getQusDuration(this.timeLeft)
+      setTimeout(() => {
+        this.timeLeft =  this.qusInfo[this.activequs].questionDetails.duration * 60;
+        this.counterStart.begin();
+       }, 0);
     }else {
       this.toast.warning('No '+status+' question..')
     }
     this.actions(status, false,this.activequs);
   }
 
-  // skipQus(skipqus,status){
-  //   if(this.isRecordStarted == true){
-  //     this.isRecordStarted = false;
-  //     this.qusEndDate = new Date();
 
-  //       }else{
-  //         this.qusStartDate = '';
-  //       }
-  //           this.isStartbtn = true; 
-  //           if(this.qusInfo.length > skipqus){
-  //             this.qusDetails = this.qusInfo[skipqus].questionDetails._id;
-  //             this.nextQusId = this.qusInfo[skipqus+ 1].questionDetails._id;
-  //             this.qusDuration = this.qusInfo[skipqus + 1].questionDetails.duration * 60;
-  //             this.activequs = skipqus + 1;
-  //             this.timeLeft =  this.qusInfo[this.activequs].questionDetails.duration * 60;
-  //             this.actions(status,false, this.activequs);
-  //           }else {
-  //             this.toast.warning('No question to skip..')
-  //           }
-  // }
-
-  // previousQus(previousQus,status){
-  //   if(this.isRecordStarted == true){
-  //     this.isRecordStarted = false;
-  //     this.qusEndDate = new Date();
-
-  //     }else{
-  //       this.qusStartDate = '';
-  //     }
-  //   this.isStartbtn = true; 
-  //   if(previousQus > 0){
-  //     this.qusDetails = this.qusInfo[previousQus].questionDetails._id;
-  //     this.nextQusId = this.qusInfo[previousQus - 1].questionDetails._id;
-  //     this.qusDuration = this.qusInfo[previousQus - 1].questionDetails.duration * 60;
-  //     this.activequs = previousQus - 1;
-  //     this.timeLeft =  this.qusInfo[this.activequs].questionDetails.duration * 60;
-  //     this.actions(status,false, this.activequs);
-  //   }else {
-  //     this.toast.warning('No question to previous..')
-  //   }
-  // }
-
-
-  // Ans record timer event
-  onComplete($event,index){
+    // Ans record timer event
+    onComplete($event,index){
       if($event){
         if(this.qusInfo.length -1 > index){
-          // alert('Please press ok to move next question');
           this.toast.success('Question time expired moving to next question')
-            this.isStartbtn = true; 
-            // this.isNextBtn = false;
-            // this.timeLeft = 0;
-            // this.countdownStart = 1;
-           
+            this.isStartbtn = false; 
             this.activequs = parseInt(this.activequs) + 1;
             sessionStorage.setItem('activequs', this.activequs + 1)
             this.displayQus = this.qusInfo[this.activequs].questionDetails.question
             this.timeLeft =  this.qusInfo[this.activequs].questionDetails.duration * 60;
+            this.getQusDuration(this.timeLeft)
             this.nextQusId = this.qusInfo[this.activequs].questionDetails._id;
             this.qusDuration = this.qusInfo[this.activequs].questionDetails.duration * 60;
             this.qusEndTime = new Date();
-            this.actions('auto complete',true,'')
+            this.actions('auto complete',true,'');
+            this.countdownStart = null;
+            setTimeout(() => {
+              this.timeLeft =  this.qusInfo[this.activequs].questionDetails.duration * 60;
+              this.getQusDuration(this.timeLeft)
+              this.counterStart.begin();
+             }, 0);
           }else {
             this.actions('submit',false,'')
             this.toast.warning('No Next question..')
@@ -222,10 +168,6 @@ export class VideoInterviewComponent implements OnInit {
   onStart($event){
     console.log($event)
   }
-
-
-
-
 onSubmit(activequs,stauts){
   this.qusDetails = this.qusInfo[activequs].questionDetails._id;
   this.qusDuration = this.qusInfo[activequs].questionDetails.duration * 60;
@@ -282,18 +224,23 @@ onSubmit(activequs,stauts){
               if(request.action == 'submit'){
                   this.openDialog()
               }
-              this.timeLeft = response.data.timeLeft ? response.data.timeLeft : this.timeLeft;
+              // this.timeLeft = response.data.timeLeft ? response.data.timeLeft : this.timeLeft;
               if(response.data.timeLeft <=0){
                 this.timeLeft = 0;
-                this.countdownStart = 0;
+                this.countdownStart = 1;
                 this.counterStart.stop();
                 this.toast.warning('Question time expired');
                 this.isStartEnable = true;
               }else{
+                // this.timeLeft = 0;
+                // this.countdownStart = 1;
                 this.countdownStart = response.data.duration ? response.data.duration : 1;
+                this.timeLeft = response.data.timeLeft ? response.data.timeLeft : this.timeLeft;
+                this.getQusDuration(this.timeLeft)
                 if(restart){
                   this.counterStart.begin();
                   setTimeout(() => {
+                    this.countdownStart = 1;
                    this.counterStart.begin();
                   }, 0);
                 }
@@ -319,6 +266,76 @@ onSubmit(activequs,stauts){
 
 
     handleEvent(event:any) {
+        console.log(event)
+    }
 
+
+    getQusDuration(qustime){
+      const duration = moment.duration(qustime, 'seconds');
+      const resultstring = moment.utc(duration.asMilliseconds()).format('HH:mm:ss');
+      this.qusTime = resultstring;
     }
 }
+
+
+
+  // getTime(duration,index,qusLength,activequs){
+  //   this.activequs = index; // display active qus button
+  //   this.isStartbtn = true; // start button enable disable
+  //   this.timeLeft = parseInt(duration) * 60;  // convert mins into secx
+  //   this.countdownStart = 1;
+  //   this.qusDetails = this.qusInfo[activequs].questionDetails._id;
+  //   this.nextQusId = this.qusInfo[activequs+ 1].questionDetails._id;
+  //   this.qusDuration = duration * 60;
+  //   if(qusLength -1 == index){
+  //     // this.actions('submit',false,activequs);
+  //   }else{
+  //     this.actions('next', false,activequs);
+  //   }
+  // }
+
+
+
+  // skipQus(skipqus,status){
+  //   if(this.isRecordStarted == true){
+  //     this.isRecordStarted = false;
+  //     this.qusEndDate = new Date();
+
+  //       }else{
+  //         this.qusStartDate = '';
+  //       }
+  //           this.isStartbtn = true; 
+  //           if(this.qusInfo.length > skipqus){
+  //             this.qusDetails = this.qusInfo[skipqus].questionDetails._id;
+  //             this.nextQusId = this.qusInfo[skipqus+ 1].questionDetails._id;
+  //             this.qusDuration = this.qusInfo[skipqus + 1].questionDetails.duration * 60;
+  //             this.activequs = skipqus + 1;
+  //             this.timeLeft =  this.qusInfo[this.activequs].questionDetails.duration * 60;
+  //             this.actions(status,false, this.activequs);
+  //           }else {
+  //             this.toast.warning('No question to skip..')
+  //           }
+  // }
+
+  // previousQus(previousQus,status){
+  //   if(this.isRecordStarted == true){
+  //     this.isRecordStarted = false;
+  //     this.qusEndDate = new Date();
+
+  //     }else{
+  //       this.qusStartDate = '';
+  //     }
+  //   this.isStartbtn = true; 
+  //   if(previousQus > 0){
+  //     this.qusDetails = this.qusInfo[previousQus].questionDetails._id;
+  //     this.nextQusId = this.qusInfo[previousQus - 1].questionDetails._id;
+  //     this.qusDuration = this.qusInfo[previousQus - 1].questionDetails.duration * 60;
+  //     this.activequs = previousQus - 1;
+  //     this.timeLeft =  this.qusInfo[this.activequs].questionDetails.duration * 60;
+  //     this.actions(status,false, this.activequs);
+  //   }else {
+  //     this.toast.warning('No question to previous..')
+  //   }
+  // }
+
+
