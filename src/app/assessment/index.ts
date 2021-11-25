773643a384128dc33cd09e2483a89b70aac229b6
B-ANKIT <b-ankit@lntecc.com>
2021-11-25 13:53:33 +0530

    Commit with system check UI issues fixes

--- src/app/assessment/index.ts
+++ src/app/assessment/index.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
 import { NavBarModule } from '../shared/navbar';
 import { AssessmentRoutingModule } from './assessment-routing.module';
 import { AssessmentComponent } from './assessment.component';
 @NgModule({
-  imports: [CommonModule, AssessmentRoutingModule, NavBarModule],
+  imports: [CommonModule, AssessmentRoutingModule, NavBarModule,MaterialModule],
   declarations: [AssessmentComponent]
 })
 export class AssessmentModule {}
